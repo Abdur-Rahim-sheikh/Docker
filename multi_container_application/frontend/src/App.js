b547d310c44b5ac4951b99e2e4e50436056aad35
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2024-03-18 08:26:30 +0600

    container name changed back to localhost cause react runs in browser so it does not know about any container

--- multi_container_application/frontend/src/App.js
+++ multi_container_application/frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
       setIsLoading(true);
 
       try {
-        const response = await fetch('http://backend-app/goals');
+        const response = await fetch('http://localhost/goals');
 
         const resData = await response.json();
 
@@ -39,7 +39,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://backend-app/goals', {
+      const response = await fetch('http://localhost/goals', {
         method: 'POST',
         body: JSON.stringify({
           text: goalText,
@@ -78,7 +78,7 @@ function App() {
     setIsLoading(true);
 
     try {
-      const response = await fetch('http://backend-app/goals/' + goalId, {
+      const response = await fetch('http://localhost/goals/' + goalId, {
         method: 'DELETE',
       });
 
