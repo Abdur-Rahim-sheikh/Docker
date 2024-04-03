7e2720f72667f1ce5a6fbf40023a441cd3f02091
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2024-04-03 12:31:51 +0600

    debugged multi-container deployment

--- deployment-04-multi-container-starting-setup/backend/app.js
+++ deployment-04-multi-container-starting-setup/backend/app.js
@@ -76,10 +76,11 @@ app.delete('/goals/:id', async (req, res) => {
 });
 
 mongoose.connect(
-  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/course-goals?authSource=admin`,
+  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}:27017/course-goals?authSource=admin`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    family: 4
   },
   (err) => {
     if (err) {
