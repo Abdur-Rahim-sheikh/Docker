df5427e2d65c79d4861377a901fd5e87e1dc0f65
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2024-04-08 12:02:24 +0600

    mongo atlas deployed

--- deployment-04-multi-container-starting-setup/backend/app.js
+++ deployment-04-multi-container-starting-setup/backend/app.js
@@ -76,7 +76,8 @@ app.delete('/goals/:id', async (req, res) => {
 });
 
 mongoose.connect(
-  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}:27017/course-goals?authSource=admin`,
+  // `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}:27017/course-goals?authSource=admin`,
+  `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_NAME}?retryWrites=true&w=majority&appName=Cluster0`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
