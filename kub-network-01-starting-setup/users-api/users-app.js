e5d4acdd35969830df4aeb925227ccaa258daafe
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2024-04-29 10:31:45 +0600

    url changed so kubernetes can connect

--- kub-network-01-starting-setup/users-api/users-app.js
+++ kub-network-01-starting-setup/users-api/users-app.js
@@ -23,8 +23,8 @@ app.post('/signup', async (req, res) => {
   }
 
   try {
-    // const hashedPW = await axios.get('http://auth/hashed-password/' + password);
-    const hashedPW = "dummy text";
+    const hashedPW = await axios.get(`http://${process.env.AUTH_ADDRESS}/`+'/hashed-password/' + password);
+    // const hashedPW = "dummy text";
     
     // since it's a dummy service, we don't really care for the hashed-pw either
     console.log(hashedPW, email);
@@ -55,10 +55,10 @@ app.post('/login', async (req, res) => {
 
   // normally, we'd find a user by email and grab his/ her ID and hashed password
   const hashedPassword = password + '_hash';
-  // const response = await axios.get(
-  //   'http://auth/token/' + hashedPassword + '/' + password
-  // );
-  const response = {status:200, data:{token: 'abc'}};
+  const response = await axios.get(
+    `http://${process.env.AUTH_ADDRESS}/token/` + hashedPassword + '/' + password
+  );
+  // const response = {status:200, data:{token: 'abc'}};
   if (response.status === 200) {
     return res.status(200).json({ token: response.data.token });
   }
